2387d5eb828bdfe6127db34fd2c26f04ad965870
NeuralIO444 <3093834+NeuralIO444@users.noreply.github.com>
2026-09-18 16:22:47 -0700

    Asset library Phase A: 48 canon assets (#281)
    Fills crystalline, biosynthetic, fragments, scanlines. Closes #281.

--- app/src/data/assets/index.js
+++ app/src/data/assets/index.js
@@ -2,5 +2,6 @@
 import { ASSETS_PART1 } from './assets-organic-geo.js';
 import { ASSETS_PART2 } from './assets-line-floral.js';
 import { ASSETS_PART3 } from './assets-stamp-radial.js';
+import { ASSETS_PART4 } from './assets-experimental.js';
 
-export const ASSETS = [...ASSETS_PART1, ...ASSETS_PART2, ...ASSETS_PART3];
+export const ASSETS = [...ASSETS_PART1, ...ASSETS_PART2, ...ASSETS_PART3, ...ASSETS_PART4];
